a7b056f1ca2625c4127755ee8404d3f91cf3bdd8
syarifsenong <syarif.nurhid@gmail.com>
2020-09-19 09:16:50 +0700

    panggil method view di class About.php

--- app/controllers/About.php
+++ app/controllers/About.php
@@ -5,11 +5,13 @@ class About
 
     public function index($nama = 'Syarif', $pekerjaan = 'Dev ') //ambil input params dari url
     {
-        echo "Halo nama saya $nama, saya adalah seorang $pekerjaan";
+        // echo "Halo nama saya $nama, saya adalah seorang $pekerjaan";
+        $this->view('about/index'); //panggil view dar method view di class controller
     }
 
     public function page()
     {
-        echo 'about/page';
+        // echo 'about/page';
+        $this->view('about/page'); //panggil view dar method view di class controller
     }
 }
