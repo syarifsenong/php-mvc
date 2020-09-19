f218afc71e729464f606d0e65fc425d4664ab08c
syarifsenong <syarif.nurhid@gmail.com>
2020-09-19 09:21:07 +0700

    menambahkan text title dan h1

--- app/views/about/index.php
+++ app/views/about/index.php
@@ -4,11 +4,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Halaman About</title>
 </head>
 
 <body>
-    <h1>Halaman About</h1>
+    <h1>About Me</h1>
 </body>
 
 </html>
