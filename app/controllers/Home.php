dc405b52788de331c0139f205fed808c1a266dfa
syarifsenong <syarif.nurhid@gmail.com>
2020-09-19 14:03:31 +0700

    buat pemanggilan model di controller home

--- app/controllers/Home.php
+++ app/controllers/Home.php
@@ -5,9 +5,10 @@ class Home extends Controller //inheritance dari class Controller
     public function index()
     {
         $data['judul'] = 'Home'; //kirim judul ke view header
+        $data['nama'] = $this->model('User_model')->getUser();
         //manggil file yg ada difolder views, home, dan file index.php
         $this->view('templates/header', $data); //manggil template header
-        $this->view('home/index');
+        $this->view('home/index', $data);
         $this->view('templates/footer'); //manggil template footer
     }
 }
