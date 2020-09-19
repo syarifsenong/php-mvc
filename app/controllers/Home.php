0e427fdfa348040b2c0171ec0b1aa295abe02c70
syarifsenong <syarif.nurhid@gmail.com>
2020-09-19 09:58:02 +0700

    manggil templates di controller Home

--- app/controllers/Home.php
+++ app/controllers/Home.php
@@ -5,6 +5,8 @@ class Home extends Controller //inheritance dari class Controller
     public function index()
     {
         //manggil file yg ada difolder views, home, dan file index.php
+        $this->view('templates/header'); //manggil template header
         $this->view('home/index');
+        $this->view('templates/footer'); //manggil template footer
     }
 }
