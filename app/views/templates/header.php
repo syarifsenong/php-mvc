c89857c6a4d8965157d495db026bd505b2fd9954
syarifsenong <syarif.nurhid@gmail.com>
2020-09-19 14:14:37 +0700

    buat navbar Karyawan

--- app/views/templates/header.php
+++ app/views/templates/header.php
@@ -20,6 +20,7 @@
             <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
                 <div class="navbar-nav">
                     <a class="nav-link active" href="<?= BASEURL; ?>/home">Home <span class="sr-only">(current)</span></a>
+                    <a class="nav-link" href="<?= BASEURL; ?>/karyawan">Karyawan</a>
                     <a class="nav-link" href="<?= BASEURL; ?>/about">About</a>
                 </div>
             </div>
