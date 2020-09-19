bac33a2eb5ea1991ffa34f55fbe9689364914708
syarifsenong <syarif.nurhid@gmail.com>
2020-09-19 13:34:57 +0700

    merapikan dan menambahkan gambar

--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,3 +1,6 @@
-<h1>About Me</h1>
-<!-- ambil data params dari controller About.php -->
-<p>Halo, nama saya <?= $data['nama']; ?>, pekerjaan saya <?= $data['pekerjaan']; ?>.</p>
+<div class="container">
+    <h1 class="mt-4">About Me</h1>
+    <img src="<?= BASEURL; ?>/img/gambar.jpg" alt="Syarif" width="200" class="rounded-circle shadow mb-3">
+    <!-- ambil data params dari controller About.php -->
+    <p>Halo, nama saya <?= $data['nama']; ?>, pekerjaan saya <?= $data['pekerjaan']; ?>.</p>
+</div>
