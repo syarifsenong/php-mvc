1081b2349bd92c2f90510dc131238c5b7c931713
syarifsenong <syarif.nurhid@gmail.com>
2020-09-19 13:11:35 +0700

    menambah folder, link ke css dan js

--- app/views/templates/header.php
+++ app/views/templates/header.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- terima data judul dari controller -->
     <title>Halaman <?= $data['judul']; ?></title>
+    <link rel="stylesheet" href="http://localhost/php-mvc/public/css/bootstrap.css">
 </head>
 
 <body>
